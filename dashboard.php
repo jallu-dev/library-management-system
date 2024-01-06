23c007bfe04c48dace03731837c6610426979eb4
Liyasdeen Mohammed Zahran <zzzahrannnldeen@gmail.com>
2024-01-06 22:39:01 +0530

    Hide header when loggedout

--- dashboard.php
+++ dashboard.php
@@ -4,59 +4,59 @@ include('includes/config.php');
 if (strlen($_SESSION['alogin']) == 0) {
     header('location:index.php');
 } else { ?>
-<!DOCTYPE html>
+    <!DOCTYPE html>
 
-<head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-    <meta name="description" content="" />
-    <meta name="author" content="" />
-    <!--[if IE]>
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <!--[if IE]>
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <![endif]-->
-    <title>Online Library Management System | Admin Dash Board</title>
-    <!-- BOOTSTRAP CORE STYLE  -->
-    <link href="assets/css/bootstrap.css" rel="stylesheet" />
-    <!-- FONT AWESOME STYLE  -->
-    <link href="assets/css/font-awesome.css" rel="stylesheet" />
-    <!-- CUSTOM STYLE  -->
-    <link href="assets/css/style.css" rel="stylesheet" />
-    <!-- GOOGLE FONT -->
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+        <title>Online Library Management System | Admin Dash Board</title>
+        <!-- BOOTSTRAP CORE STYLE  -->
+        <link href="assets/css/bootstrap.css" rel="stylesheet" />
+        <!-- FONT AWESOME STYLE  -->
+        <link href="assets/css/font-awesome.css" rel="stylesheet" />
+        <!-- CUSTOM STYLE  -->
+        <link href="assets/css/style.css" rel="stylesheet" />
+        <!-- GOOGLE FONT -->
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
 
-    <style>
-    .carousel.slide.slide-bdr,
-    .carousel-inner,
-    .item {
-        height: 500px;
-    }
+        <style>
+            .carousel.slide.slide-bdr,
+            .carousel-inner,
+            .item {
+                height: 500px;
+            }
 
-    .carousel-inner img {
-        height: 100%;
-        width: 100%;
-    }
-    </style>
-</head>
+            .carousel-inner img {
+                height: 100%;
+                width: 100%;
+            }
+        </style>
+    </head>
 
-<body>
-    <!------MENU SECTION START-->
-    <?php include('includes/header.php'); ?>
-    <!-- MENU SECTION END-->
-    <div class="content-wrapper">
-        <div class="container">
-            <div class="row pad-botm">
-                <div class="col-md-12">
-                    <h4 class="header-line">ADMIN DASHBOARD</h4>
+    <body>
+        <!------MENU SECTION START-->
+        <?php include('includes/header.php'); ?>
+        <!-- MENU SECTION END-->
+        <div class="content-wrapper">
+            <div class="container">
+                <div class="row pad-botm">
+                    <div class="col-md-12">
+                        <h4 class="header-line">ADMIN DASHBOARD</h4>
+                    </div>
+
                 </div>
 
-            </div>
+                <div class="row">
 
-            <div class="row">
-
-                <div class="col-md-3 col-sm-3 col-xs-6">
-                    <div class="alert alert-success back-widget-set text-center">
-                        <i class="fa fa-book fa-5x"></i>
-                        <?php
+                    <div class="col-md-3 col-sm-3 col-xs-6">
+                        <div class="alert alert-success back-widget-set text-center">
+                            <i class="fa fa-book fa-5x"></i>
+                            <?php
                             $sql = "SELECT id from tblbooks ";
                             $result = $conn->query($sql);
 
@@ -64,72 +64,72 @@ if (strlen($_SESSION['alogin']) == 0) {
                             ?>
 
 
-                        <h3>
-                            <?php echo htmlentities($listdbooks); ?>
-                        </h3>
-                        Books Listed
+                            <h3>
+                                <?php echo htmlentities($listdbooks); ?>
+                            </h3>
+                            Books Listed
+                        </div>
                     </div>
-                </div>
 
 
-                <div class="col-md-3 col-sm-3 col-xs-6">
-                    <div class="alert alert-info back-widget-set text-center">
-                        <i class="fa fa-bars fa-5x"></i>
-                        <?php
+                    <div class="col-md-3 col-sm-3 col-xs-6">
+                        <div class="alert alert-info back-widget-set text-center">
+                            <i class="fa fa-bars fa-5x"></i>
+                            <?php
                             $sql1 = "SELECT id from tblissuedbookdetails ";
                             $result1 = $conn->query($sql1);
 
                             $issuedbooks = $result1->num_rows;
                             ?>
 
-                        <h3>
-                            <?php echo htmlentities($issuedbooks); ?>
-                        </h3>
-                        Times Book Issued
+                            <h3>
+                                <?php echo htmlentities($issuedbooks); ?>
+                            </h3>
+                            Times Book Issued
+                        </div>
                     </div>
-                </div>
 
-                <div class="col-md-3 col-sm-3 col-xs-6">
-                    <div class="alert alert-warning back-widget-set text-center">
-                        <i class="fa fa-recycle fa-5x"></i>
-                        <?php
+                    <div class="col-md-3 col-sm-3 col-xs-6">
+                        <div class="alert alert-warning back-widget-set text-center">
+                            <i class="fa fa-recycle fa-5x"></i>
+                            <?php
                             $status = 1;
                             $sql2 = "SELECT id from tblissuedbookdetails where RetrunStatus='{$status}'";
                             $result2 = $conn->query($sql2);
                             $returnedbooks = $result2->num_rows;
                             ?>
 
-                        <h3>
-                            <?php echo htmlentities($returnedbooks); ?>
-                        </h3>
-                        Times Books Returned
+                            <h3>
+                                <?php echo htmlentities($returnedbooks); ?>
+                            </h3>
+                            Times Books Returned
+                        </div>
                     </div>
-                </div>
-                <div class="col-md-3 col-sm-3 col-xs-6">
-                    <div class="alert alert-danger back-widget-set text-center">
-                        <i class="fa fa-users fa-5x"></i>
-                        <?php
+                    <div class="col-md-3 col-sm-3 col-xs-6">
+                        <div class="alert alert-danger back-widget-set text-center">
+                            <i class="fa fa-users fa-5x"></i>
+                            <?php
                             $sql3 = "SELECT id from tblstudents ";
                             $result3 = $conn->query($sql3);
                             $regstds = $result3->num_rows;
                             ?>
-                        <h3>
-                            <?php echo htmlentities($regstds); ?>
-                        </h3>
-                        Registered Users
+                            <h3>
+                                <?php echo htmlentities($regstds); ?>
+                            </h3>
+                            Registered Users
+                        </div>
                     </div>
+
                 </div>
 
-            </div>
 
 
+                <div class="row">
 
-            <div class="row">
-
-                <div class="col-md-3 col-sm-3 col-xs-6">
-                    <div class="alert alert-success back-widget-set text-center">
-                        <i class="fa fa-user fa-5x"></i>
-                        <?php
+                    <div class="col-md-3 col-sm-3 col-xs-6">
+                        <div class="alert alert-success back-widget-set text-center">
+                            <i class="fa fa-user fa-5x"></i>
+                            <?php
                             $sql4 = "SELECT id from tblauthors ";
                             $result4 = $conn->query($sql4);
 
@@ -137,99 +137,99 @@ if (strlen($_SESSION['alogin']) == 0) {
                             ?>
 
 
-                        <h3>
-                            <?php echo htmlentities($listdathrs); ?>
-                        </h3>
-                        Authors Listed
+                            <h3>
+                                <?php echo htmlentities($listdathrs); ?>
+                            </h3>
+                            Authors Listed
+                        </div>
                     </div>
-                </div>
 
 
-                <div class="col-md-3 col-sm-3 rscol-xs-6">
-                    <div class="alert alert-info back-widget-set text-center">
-                        <i class="fa fa-file-archive-o fa-5x"></i>
-                        <?php
+                    <div class="col-md-3 col-sm-3 rscol-xs-6">
+                        <div class="alert alert-info back-widget-set text-center">
+                            <i class="fa fa-file-archive-o fa-5x"></i>
+                            <?php
                             $sql5 = "SELECT id from tblcategory ";
                             $result5 = $conn->query($sql5);
 
                             $listdcats = $result5->num_rows;
                             ?>
 
-                        <h3>
-                            <?php echo htmlentities($listdcats); ?>
-                        </h3>
-                        Listed Categories
-                    </div>
-                </div>
-
-
-            </div>
-
-            <div class="row">
-
-                <div class="col-md-10 col-sm-8 col-xs-12 col-md-offset-1">
-                    <div id="carousel-example" class="carousel slide slide-bdr" data-ride="carousel">
-
-                        <div class="carousel-inner">
-                            <div class="item active">
-
-                                <img src="assets/img/1.jpg" alt="" />
-
-                            </div>
-                            <div class="item">
-                                <img src="assets/img/2.jpg" alt="" />
-
-                            </div>
-                            <div class="item">
-                                <img src="assets/img/3.jpg" alt="" />
-
-                            </div>
-                            <div class="item active">
-
-                                <img src="assets/img/4.jpg" alt="" />
-
-                            </div>
-
-                            <div class="item">
-                                <img src="assets/img/6.jpg" alt="" />
-
-                            </div>
+                            <h3>
+                                <?php echo htmlentities($listdcats); ?>
+                            </h3>
+                            Listed Categories
                         </div>
-                        <!--INDICATORS-->
-                        <ol class="carousel-indicators">
-                            <li data-target="#carousel-example" data-slide-to="0" class="active"></li>
-                            <li data-target="#carousel-example" data-slide-to="1"></li>
-                            <li data-target="#carousel-example" data-slide-to="2"></li>
-                            <li data-target="#carousel-example" data-slide-to="3"></li>
-                            <li data-target="#carousel-example" data-slide-to="4"></li>
-                        </ol>
-                        <!--PREVIUS-NEXT BUTTONS-->
-                        <a class="left carousel-control" href="#carousel-example" data-slide="prev">
-                            <span class="glyphicon glyphicon-chevron-left"></span>
-                        </a>
-                        <a class="right carousel-control" href="#carousel-example" data-slide="next">
-                            <span class="glyphicon glyphicon-chevron-right"></span>
-                        </a>
                     </div>
+
+
                 </div>
 
+                <div class="row">
+
+                    <div class="col-md-10 col-sm-8 col-xs-12 col-md-offset-1">
+                        <div id="carousel-example" class="carousel slide slide-bdr" data-ride="carousel">
+
+                            <div class="carousel-inner">
+                                <div class="item active">
+
+                                    <img src="assets/img/1.jpg" alt="" />
+
+                                </div>
+                                <div class="item">
+                                    <img src="assets/img/2.jpg" alt="" />
+
+                                </div>
+                                <div class="item">
+                                    <img src="assets/img/3.jpg" alt="" />
+
+                                </div>
+                                <div class="item active">
+
+                                    <img src="assets/img/4.jpg" alt="" />
+
+                                </div>
+
+                                <div class="item">
+                                    <img src="assets/img/6.jpg" alt="" />
+
+                                </div>
+                            </div>
+                            <!--INDICATORS-->
+                            <ol class="carousel-indicators">
+                                <li data-target="#carousel-example" data-slide-to="0" class="active"></li>
+                                <li data-target="#carousel-example" data-slide-to="1"></li>
+                                <li data-target="#carousel-example" data-slide-to="2"></li>
+                                <li data-target="#carousel-example" data-slide-to="3"></li>
+                                <li data-target="#carousel-example" data-slide-to="4"></li>
+                            </ol>
+                            <!--PREVIUS-NEXT BUTTONS-->
+                            <a class="left carousel-control" href="#carousel-example" data-slide="prev">
+                                <span class="glyphicon glyphicon-chevron-left"></span>
+                            </a>
+                            <a class="right carousel-control" href="#carousel-example" data-slide="next">
+                                <span class="glyphicon glyphicon-chevron-right"></span>
+                            </a>
+                        </div>
+                    </div>
 
 
 
+
+
+                </div>
 
             </div>
-
         </div>
-    </div>
-    <!-- JAVASCRIPT FILES PLACED AT THE BOTTOM TO REDUCE THE LOADING TIME  -->
-    <!-- CORE JQUERY  -->
-    <script src="assets/js/jquery-1.10.2.js"></script>
-    <!-- BOOTSTRAP SCRIPTS  -->
-    <script src="assets/js/bootstrap.js"></script>
-    <!-- CUSTOM SCRIPTS  -->
-    <script src="assets/js/custom.js"></script>
-</body>
+        <!-- JAVASCRIPT FILES PLACED AT THE BOTTOM TO REDUCE THE LOADING TIME  -->
+        <!-- CORE JQUERY  -->
+        <script src="assets/js/jquery-1.10.2.js"></script>
+        <!-- BOOTSTRAP SCRIPTS  -->
+        <script src="assets/js/bootstrap.js"></script>
+        <!-- CUSTOM SCRIPTS  -->
+        <script src="assets/js/custom.js"></script>
+    </body>
 
-</html>
+    </html>
 
 <?php } ?>
---
+++ signup.php
@@ -0,0 +1,143 @@
+<?php
+session_start();
+include('includes/config.php');
+if (isset($_POST['signup'])) {
+
+    $count_my_page = "studentid.txt";
+    $hits = file($count_my_page);
+    $hits[0]++;
+    $fp = fopen($count_my_page, "w");
+    fputs($fp, "$hits[0]");
+    fclose($fp);
+    $StudentId = "SID{$hits[0]}";
+    $fname = $_POST['fullanme'];
+    $mobileno = $_POST['mobileno'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $confirmpassword = $_POST['confirmpassword'];
+    $status = 1;
+
+    $sqlCheckEmail = "SELECT `EmailId` FROM `tblstudents` WHERE EmailId='{$email}'";
+
+    if ($conn->query($sqlCheckEmail)->num_rows <= 0) {
+        if ($password == $confirmpassword) {
+            echo $StudentId;
+            $sql = "INSERT INTO tblstudents(StudentId,FullName,MobileNumber,EmailId,Password,Status) VALUES('{$StudentId}','{$fname}','{$mobileno}','{$email}','{$password}',{$status})";
+            echo $result;
+            $result = $conn->query($sql);
+            $lastInsertId = $conn->insert_id;
+            if ($lastInsertId) {
+                echo '<script>alert("Student Registration successfull and student id is  "+"' . $StudentId . '")</script>';
+                Header("Location:reg-students.php");
+            } else {
+                echo "<script>alert('Something went wrong. Please try again');</script>";
+            }
+        } else {
+            echo "<script>alert('Password and Confirm password not matched');</script>";
+        }
+    } else {
+        echo "<script>alert('User already exists');</script>";
+    }
+
+}
+?>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <!--[if IE]>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <![endif]-->
+    <title>Online Library Management System | Student Signup</title>
+    <!-- BOOTSTRAP CORE STYLE  -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+    <!-- FONT AWESOME STYLE  -->
+    <link href="assets/css/font-awesome.css" rel="stylesheet" />
+    <!-- CUSTOM STYLE  -->
+    <link href="assets/css/style.css" rel="stylesheet" />
+    <!-- GOOGLE FONT -->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+
+
+
+</head>
+
+<body>
+    <!------MENU SECTION START-->
+    <?php include('includes/header.php'); ?>
+    <!-- MENU SECTION END-->
+    <div class="content-wrapper">
+        <div class="container">
+            <div class="row pad-botm">
+                <div class="col-md-12">
+                    <h4 class="header-line">User Signup</h4>
+
+                </div>
+
+            </div>
+            <div class="row">
+
+                <div class="col-md-9 col-md-offset-1">
+                    <div class="panel panel-danger">
+                        <div class="panel-heading">
+                            SINGUP FORM
+                        </div>
+                        <div class="panel-body">
+                            <form name="signup" method="post" onSubmit="return valid();">
+                                <div class="form-group">
+                                    <label>Enter Full Name</label>
+                                    <input class="form-control" type="text" name="fullanme" autocomplete="off"
+                                        required />
+                                </div>
+
+
+                                <div class="form-group">
+                                    <label>Mobile Number :</label>
+                                    <input class="form-control" type="text" name="mobileno" maxlength="10"
+                                        autocomplete="off" required />
+                                </div>
+
+                                <div class="form-group">
+                                    <label>Enter Email</label>
+                                    <input class="form-control" type="email" name="email" id="emailid"
+                                        onBlur="checkAvailability()" autocomplete="off" required />
+                                    <span id="user-availability-status" style="font-size:12px;"></span>
+                                </div>
+
+                                <div class="form-group">
+                                    <label>Enter Password</label>
+                                    <input class="form-control" type="password" name="password" autocomplete="off"
+                                        required />
+                                </div>
+
+                                <div class="form-group">
+                                    <label>Confirm Password </label>
+                                    <input class="form-control" type="password" name="confirmpassword"
+                                        autocomplete="off" required />
+                                </div>
+
+                                <button type="submit" name="signup" class="btn btn-danger" id="submit">Register Now
+                                </button>
+
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- CONTENT-WRAPPER SECTION END-->
+    <?php include('includes/footer.php'); ?>
+    <script src="assets/js/jquery-1.10.2.js"></script>
+    <!-- BOOTSTRAP SCRIPTS  -->
+    <script src="assets/js/bootstrap.js"></script>
+    <!-- CUSTOM SCRIPTS  -->
+    <script src="assets/js/custom.js"></script>
+</body>
+
+</html>
